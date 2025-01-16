53474dd1aa0b4bd7bf8946397c5bcc65513527f8
Saeed Jeddi <saeedjeddidev@gmail.com>
2025-01-16 11:30:29 +0330

    rtl

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,101 @@
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Estedad',
+      ),
       debugShowCheckedModeBanner: false,
       title: 'My App',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        // Locale('en'), // English
+        Locale('fa'), // persian
+      ],
       home: Scaffold(
-        appBar: AppBar(
-          actions: [
-              SizedBox(width: 8,),
-              Expanded(child: Align(alignment: Alignment.topLeft,child: Padding(
-                padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
-                child: Image.asset('assets/images/menu.png', width: 20,height: 20,),
-              ))),
+          appBar: AppBar(
+            actions: [
+              SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                  child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
+                        child: Image.asset(
+                          'assets/images/menu.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                      ))),
               Padding(
                 padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
-                child: Text('قیمت بروز ارزها', textDirection: TextDirection.rtl, style: TextStyle(color: Colors.black),),
+                child: Text(
+                  'قیمت بروز ارزها',
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(color: Colors.black),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
-                child: Image.asset('assets/images/icon.png', width: 32,height: 32,),
-                
+                child: Image.asset(
+                  'assets/images/icon.png',
+                  width: 32,
+                  height: 32,
+                ),
               ),
-          ],
-          ),
-
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            
-            children: [
-              Row(
-                
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  
-                  Text('نرخ ارز آزاد ',),
-                  SizedBox(width: 8,),
-                  Image.asset('assets/images/12.png', width: 15,height: 15,),
-                ],
-              ),
-              SizedBox(height: 8,),
-              Text("برای اطلاع از نرخ ارز آزاد امروز به اینجا مراجعه کنید تا در لحظه از قیمت ها با خبر شوید .", textDirection: TextDirection.rtl, style: TextStyle(color: Colors.red, fontSize: 12),),
-              SizedBox(height: 8,),
-              Row(
-                
-              ),
-
             ],
           ),
-        )
-        ),
-      );
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'نرخ ارز آزاد ',
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Image.asset(
+                      'assets/images/12.png',
+                      width: 15,
+                      height: 15,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "برای اطلاع از نرخ ارز آزاد امروز به اینجا مراجعه کنید تا در لحظه از قیمت ها با خبر شوید .",
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(
+                      color: Colors.red, fontSize: 12, fontFamily: 'Tanha'),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Row(),
+              ],
+            ),
+          )),
+    );
   }
-
-
 }
