91c079618ea87fb351f68c33d9188454398c3469
Saeed Jeddi <saeedjeddidev@gmail.com>
2025-01-18 22:33:50 +0330

    update 4

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,193 +17,204 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       title: 'My App',
-      localizationsDelegates: [
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: [
-        // Locale('en'), // English
-        Locale('fa'), // persian
+      supportedLocales: const [
+        Locale('fa'), // Persian
       ],
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 245, 246, 255),
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
-              child: Image.asset(
-                'assets/images/icon.png',
-                width: 32,
-                height: 32,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 245, 246, 255),
+      appBar: AppBar(
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
+            child: Image.asset(
+              'assets/images/icon.png',
+              width: 32,
+              height: 32,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(8, 16, 8, 0),
+            child: Text(
+              'قیمت بروز ارزها',
+              textDirection: TextDirection.rtl,
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Expanded(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
+                child: Image.asset(
+                  'assets/images/menu.png',
+                  width: 20,
+                  height: 20,
+                ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8, 16, 8, 0),
-              child: Text(
-                'قیمت بروز ارزها',
-                textDirection: TextDirection.rtl,
-                style: TextStyle(color: Colors.black),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/12.png',
+                  width: 15,
+                  height: 15,
+                ),
+                const SizedBox(width: 8),
+                const Text('نرخ ارز آزاد '),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "برای اطلاع از نرخ ارز آزاد امروز به اینجا مراجعه کنید تا در لحظه از قیمت ها با خبر شوید .",
+              textDirection: TextDirection.rtl,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 12,
+                fontFamily: 'Tanha',
               ),
             ),
-            SizedBox(
-              width: 8,
-            ),
-            Expanded(
-                child: Align(
-                    alignment: Alignment.topLeft,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8, 25, 0, 0),
-                      child: Image.asset(
-                        'assets/images/menu.png',
-                        width: 20,
-                        height: 20,
-                      ),
-                    ))),
-          ],
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    'assets/images/12.png',
-                    width: 15,
-                    height: 15,
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'نرخ ارز آزاد ',
-                  ),
+            const SizedBox(height: 8),
+            Container(
+              width: double.infinity,
+              height: 35,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Color.fromARGB(255, 130, 130, 130),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  Text(' نام ارز '),
+                  Text('  قیمت '),
+                  Text('  تغییرات '),
                 ],
               ),
-              SizedBox(
-                height: 8,
+            ),
+            const SizedBox(height: 8),
+            SizedBox(
+              width: double.infinity,
+              height: 430,
+              child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemCount: 8,
+                itemBuilder: (BuildContext context, int position) {
+                  return const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: MyItem(),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  if (index % 5 == 0) {
+                    return const Ads();
+                  } else {
+                    return const SizedBox(
+                      width: 0,
+                    );
+                  }
+                },
               ),
-              Text(
-                "برای اطلاع از نرخ ارز آزاد امروز به اینجا مراجعه کنید تا در لحظه از قیمت ها با خبر شوید .",
-                textDirection: TextDirection.rtl,
-                style: TextStyle(
-                    color: Colors.red, fontSize: 12, fontFamily: 'Tanha'),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              Container(
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
+              child: Container(
                 width: double.infinity,
-                height: 35,
+                height: 60,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  color: Color.fromARGB(255, 130, 130, 130),
+                  color: const Color.fromARGB(255, 232, 232, 232),
+                  borderRadius: BorderRadius.circular(50),
                 ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text(' نام ارز '),
-                    Text('  قیمت '),
-                    Text('  تغییرات '),
+                    SizedBox(
+                      height: 60,
+                      child: TextButton.icon(
+                        onPressed: () => _showSnackBar(context, " بروزرسانی انجام شد .",),
+                        icon: const Icon(
+                          CupertinoIcons.refresh_bold,
+                          size: 30,
+                        ),
+                        label: const Text('بروزرسانی'),
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(
+                            Colors.amberAccent,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                      child: Text(
+                        'زمان بروزرسانی : 2025-01-11 19:20',
+                      ),
+                    ),
                   ],
                 ),
               ),
-              SizedBox(
-                height: 8,
-              ),
-              SizedBox(
-                  width: double.infinity,
-                  height: 430,
-                  child: ListView.separated(
-                    physics: BouncingScrollPhysics(),
-                    itemCount: 8,
-                    itemBuilder: (BuildContext context, int position) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Myitem(),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) {
-                      if (index % 5 == 0) {
-                        return Ads();
-                      } else {
-                        return SizedBox(
-                          width: 0,
-                        );
-                      }
-                    },
-                  )),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
-                child: Container(
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 232, 232, 232),
-                      borderRadius: BorderRadius.circular(50)),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: 60,
-                        child: TextButton.icon(
-                          onPressed: () {},
-                          icon: Icon(
-                            CupertinoIcons.refresh_bold,
-                            size: 30,
-                          ),
-                          label: Text('بروزرسانی'),
-                          style: ButtonStyle(
-                              backgroundColor:
-                                  WidgetStateProperty.all(Colors.amberAccent)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                        child: Text('زمان بروزرسانی : 2025-01-11 19:20',),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-class Myitem extends StatelessWidget {
-  const Myitem({
-    super.key,
-  });
+void _showSnackBar(BuildContext context, String msg) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(msg),
+      backgroundColor: Colors.green,
+    ),
+  );
+}
+
+class MyItem extends StatelessWidget {
+  const MyItem({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 50,
-      decoration: BoxDecoration(boxShadow: <BoxShadow>[
-        BoxShadow(
-          blurRadius: 1.0,
-          color: Colors.grey,
-        )
-      ], color: Colors.white, borderRadius: BorderRadius.circular(50)),
+      decoration: BoxDecoration(
+        boxShadow: const <BoxShadow>[
+          BoxShadow(
+            blurRadius: 1.0,
+            color: Colors.grey,
+          )
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(
-            'دلار',
-          ),
-          Text(
-            '5200000',
-          ),
-          Text(
-            '+50',
-            textDirection: TextDirection.ltr,
-          ),
+        children: const [
+          Text('دلار'),
+          Text('5200000'),
+          Text('+50', textDirection: TextDirection.ltr),
         ],
       ),
     );
@@ -211,28 +222,25 @@ class Myitem extends StatelessWidget {
 }
 
 class Ads extends StatelessWidget {
-  const Ads({
-    super.key,
-  });
+  const Ads({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 50,
-      decoration: BoxDecoration(boxShadow: <BoxShadow>[
-        BoxShadow(
-          blurRadius: 1.0,
-          color: Colors.grey,
-        )
-      ], color: Colors.red, borderRadius: BorderRadius.circular(50)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(
-            'تبلیغات برای شما با ما تماس بگیرید .',
-          ),
+      decoration: BoxDecoration(
+        boxShadow: const <BoxShadow>[
+          BoxShadow(
+            blurRadius: 1.0,
+            color: Colors.grey,
+          )
         ],
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: const Center(
+        child: Text('تبلیغات برای شما با ما تماس بگیرید .'),
       ),
     );
   }
